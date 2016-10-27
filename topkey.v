35b0a260d0aa257b04b08221e1ff0875afe55603
Jksalas <jksalas1514@gmail.com>
2016-10-27 00:48:16 -0600

    Update topkey.v

--- topkey.v
+++ topkey.v
@@ -1,49 +1,47 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Create Date:    21:42:30 10/20/2016
-// Module Name:    top
-// Project Name:   CONTROLADOR RTC CON PICOBLAZE
-// Target Devices:  ARTIX 7
-// Tool versions:
-// Description:   modulo top para el control del teclado 
-// Additional Comments: hecho por JOAO SALAS
+
 //
+// Create Date:    21:42:30 10/20/2016
+// Design Name:
+// Module Name:    top
+
+
+// Additional Comments: hecho por JOAO SALAS
+// top para el teclado
 //////////////////////////////////////////////////////////////////////////////////
 module topkey(
   input clk,reset,
   input ps2d,ps2c, rx_en,
-  output wire [7:0] dout1,
-  output wire rx_done_tick,
-  output  a,b,c,d,up,down,left,right,
-  output  a_vga_out,b_vga_out,c_vga_out,d_vga_out,up_vga_out,down_vga_out,left_vga_out,right_vga_out
+  output a,b,c,d,up,down,left,right
+
   );
 
     wire [7:0] dout;
-	  assign dout1=dout;
-//	 wire rx_done_tick;
+    wire rx_done_tick;
 
 
+         ps2_portreceiver  PS2(
+             .clk(clk),.reset(reset),
+             .ps2d(ps2d),.ps2c(ps2c), .rx_en(rx_en),
+             .rx_done_tick(rx_done_tick),
+             .dout(dout)
+           );
 
-     ps2_portreceiver  PS2(
-     .clk(clk),.reset(reset),
-     .ps2d(ps2d),.ps2c(ps2c), .rx_en(rx_en),
-     .rx_done_tick(rx_done_tick),
-     .dout(dout)
-      );
-
-     keyout  KEY(
-        .clk(clk),.reset(reset),.rx_done_tick(rx_done_tick),
-        .keycodeout(dout),
-        .a(a),.b(b),.c(c),.d(d),.up(up),.down(down),.left(left),.right(right),
-         .a_vga_out(a_vga_out),
-         .b_vga_out(b_vga_out),
-         .c_vga_out(c_vga_out),
-         .d_vga_out(d_vga_out),
-         .up_vga_out(up_vga_out),
-         .down_vga_out(down_vga_out),
-         .left_vga_out(left_vga_out),
-         .right_vga_out(right_vga_out)
-        );
+          keyout  KEY(
+              .clk(clk),
+              .reset(reset),
+              .rx_done_tick(rx_done_tick),
+              .keycodeout(dout),
+              .a_code(a),
+              .b_code(b),
+              .c_code(c),
+              .d_code(d),
+              .up_code(up),
+              .down_code(down),
+              .left_code(left),
+              .right_code(right)
+          );
 
 
 
